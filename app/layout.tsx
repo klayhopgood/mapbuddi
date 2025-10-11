c08e3539288448a6a42b6b249beed4bfd3120875
klayhopgood <klaychop1@gmail.com>
2025-10-11 12:41:39 +1000

    fix: Move GTM and GA scripts outside html structure for proper Next.js loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,16 +18,6 @@ export default async function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <head>
-          {/* Google Tag Manager */}
-          <Script id="google-tag-manager" strategy="afterInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PDHQNPVR');
-            `}
-          </Script>
           <script
             defer
             data-domain="mapbuddi.com"
@@ -45,23 +35,35 @@ export default async function RootLayout({
             />
           </noscript>
           
-          {/* Google Analytics */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-78X0XMB9JS"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-78X0XMB9JS');
-            `}
-          </Script>
           <main>{children}</main>
           <Toaster />
         </body>
       </html>
+      
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PDHQNPVR');
+        `}
+      </Script>
+      
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-78X0XMB9JS"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-78X0XMB9JS');
+        `}
+      </Script>
     </ClerkProvider>
   );
 }
